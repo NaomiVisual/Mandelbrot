469a949e35748b89d5d7af6a3f0eb78ed0cb4ea7
jorit1290 <joritdelen@gmail.com>
2015-09-23 18:43:13 +0200

    23 september, 6:43

--- Mandelbrood/Mandelbrood/CodeFile1.cs
+++ Mandelbrood/Mandelbrood/CodeFile1.cs
@@ -19,6 +19,7 @@ namespace Mandelbrot
         {
             Label MiddenX, MiddenY, Schaal, Max;
             Button knop;
+            Panel panel;
 
             MiddenX = new Label();
             MiddenY = new Label();
@@ -29,26 +30,32 @@ namespace Mandelbrot
             this.schaal = new TextBox();
             this.max = new TextBox();
             knop = new Button();
+            panel = new Panel();
 
             MiddenX.Location = new Point(20, 20);
             MiddenY.Location = new Point(20, 45);
-            Schaal.Location = new Point(260, 20);
-            Max.Location = new Point(260, 45);
+            Schaal.Location = new Point(250, 20);
+            Max.Location = new Point(250, 45);
             this.middenx.Location = new Point(80, 17);
             this.middeny.Location = new Point(80, 42);
-            this.schaal.Location = new Point(310, 17);
-            this.max.Location = new Point(310, 42);
-            knop.Location = new Point(360, 42);
+            this.schaal.Location = new Point(300, 17);
+            this.max.Location = new Point(300, 42);
+            knop.Location = new Point(350, 42);
+            panel.Location = new Point(20, 85);
+
+            panel.BackColor = System.Drawing.Color.Black; 
 
             MiddenX.Size = new Size(60, 15);
             MiddenY.Size = new Size(60, 15);
             Schaal.Size = new Size(40, 15);
             Max.Size = new Size(40, 15);
-            this.middenx.Size = new Size(160, 15);
-            this.middeny.Size = new Size(160, 15);
+            this.middenx.Size = new Size(150, 15);
+            this.middeny.Size = new Size(150, 15);
             this.schaal.Size = new Size(90, 15);
             this.max.Size = new Size(40, 15);
             knop.Size = new Size(50, 20);
+            panel.Size = new Size(400, 400);
+
             MiddenX.Text = "Midden X:";
             MiddenY.Text = "Midden Y:";
             Schaal.Text = "Schaal:";
@@ -64,9 +71,10 @@ namespace Mandelbrot
             this.Controls.Add(schaal);
             this.Controls.Add(max);
             this.Controls.Add(knop);
+            this.Controls.Add(panel);
 
             this.Text = "Mandelbrot";
-            this.ClientSize = new Size(500, 500);
+            this.ClientSize = new Size(475, 500);
             this.BackColor = System.Drawing.Color.LightGray;
         }
 
